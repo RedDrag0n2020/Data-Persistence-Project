6a10fbfb7387c2a99d4f83f7f4f1bec683ea5643
Ken Mayfield <kdmcoder2000@gmail.com>
2026-03-05 13:54:49 -0500

    Add project files.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This script is a placeholder for the GameManager class in Unity.
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
