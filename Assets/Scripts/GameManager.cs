95a5dede6dece78be905e0a6d6d6a57c7f6a2d0a
Ken Mayfield <kdmcoder2000@gmail.com>
2026-03-05 13:56:04 -0500

    3/55/2026 Initial commit.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // This script is a placeholder for the GameManager class in Unity.
+    // This project is a learning project.
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
