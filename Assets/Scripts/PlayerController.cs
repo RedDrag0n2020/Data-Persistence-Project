e05adaf46f40f14fe8b270555aaf40ed7380ae27
Ken Mayfield <kdmcoder2000@gmail.com>
2026-04-07 14:39:16 -0400

    4/7/2026 Uppdated the PlayerController.cs script to allow turn/look functionality with the mouse.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputSystem_Actions controls;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    public float moveSpeed = 5f;
+    public float turnSpeed = 180f;
+
+    public Transform cameraTransform;
+
+    private void Awake()
     {
-        
+        controls = new InputSystem_Actions();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        controls.Player.Enable();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        controls.Player.Look.canceled += ctx => lookInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    private void Update()
+    {
+        // Rotate player horizontally
+        float turnAmount = lookInput.x * turnSpeed * Time.deltaTime;
+        transform.Rotate(0, turnAmount, 0);
+
+        // Camera-relative movement
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDir = forward * moveInput.y + right * moveInput.x;
+
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
     }
 }
